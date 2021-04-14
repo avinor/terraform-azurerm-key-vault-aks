8353997b5b58c85729f0946b96867ad14af7168a
Pavlo Prykhodko <pavlo.prykhodko@avinor.no>
2021-04-14 10:13:54 +0200

    Upgrade to terraform 0.13 syntax

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 terraform {
-  required_version = ">=0.12.23"
+  required_version = ">=0.13"
   required_providers {
     azurerm = {
+      source  = "hashicorp/azurerm"
       version = "~> 2.53.0"
     }
   }
