23d2eb32877c6f3b4d95829769de3339d9377804
Emil Kordahl <89079229+emilkor1@users.noreply.github.com>
2024-03-11 13:19:00 +0100

    Use 3.94.0

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.13"
+  required_version = ">= 0.13"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.95.0"
+      version = "~> 3.94.0"
     }
   }
 }
